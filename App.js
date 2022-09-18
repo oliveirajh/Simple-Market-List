00b4dfbe1def4e028b77b65c20203b87a84f7920
João Henrique <joaojho7@gmail.com>
2022-09-18 20:34:02 -0300

    Simple List 1.0

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import AppTab from './AppTab';
+
+export default function App() {
+  return (
+    <>
+  
+      <AppTab />
+      <StatusBar style="light" />
+
+      </>
+  );
+}
